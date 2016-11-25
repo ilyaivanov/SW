5c119c25c690c6bc50112a32432859b0109029b8
Ilya Ivanov <static.ila@gmail.com>
2016-11-25 06:14:16 +0200

    Add more unit tests

--- src/App/model/report.spec.ts
+++ src/App/model/report.spec.ts
@@ -1,10 +1,10 @@
 import * as _ from 'lodash';
 import { Answers } from '../../types/report';
 
-import { calculateTotals } from './report';
+import createReport, { calculateTotals } from './report';
 
-describe('foo', () => {
-    it('bar', () => {
+describe('When creating a report', () => {
+    it('totals should be calculated correctly', () => {
         const answers: Answers[] = [
             {
                 initialOrder: 1,
@@ -20,5 +20,28 @@ describe('foo', () => {
         expect(group.answers[1].values).toEqual(['Female', 500, 100, 600]);
         expect(group.footer).toEqual([550, 110, 660]);
     })
-    
+
+    it('Gender structure should be correct', () => {
+        const report = createReport();
+
+        expect(report.columns.length).toBe(4);
+
+        expect(report.groups[0]).toEqual({
+            name: 'Gender',
+            isCollapsed: false,
+            answers: [
+                {
+                    initialOrder: 1,
+                    values: ['Male', 1700, 130, 1830]
+                },
+                {
+                    initialOrder: 2,
+                    values: ['Female', 560, 160, 720]
+                },
+            ],
+            footer: [2260, 290, 2550]
+        })
+
+    })
+
 })
