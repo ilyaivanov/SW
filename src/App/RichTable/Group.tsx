db774c655f8fa648de1ff272053f62b81259bd9e
Ilya Ivanov <static.ila@gmail.com>
2016-11-25 06:25:37 +0200

    Refactor Group.tsx element

---
+++ src/App/RichTable/Group.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import * as cx from 'classnames';
+
+import { Group, Report, SortingIndicator } from '../../types/report';
+
+const collapsed = (isCollapsed, onGroupCollapse) => isCollapsed ?
+    <span onClick={onGroupCollapse} className="glyphicon glyphicon-plus"></span> :
+    <span onClick={onGroupCollapse} className="glyphicon glyphicon-minus"></span>;
+
+const splitedCell = (leftValue, rightValue, bordered) => (
+    <div className="wrapper">
+        <span className={cx('firstCell', { borderedCell: bordered })}>{leftValue}</span>
+        <span className="secondCell">{rightValue}</span>
+    </div>
+);
+
+const cell = (cellInfo: string | number, index: number) => {
+    const formatNumber = (cell: number) => splitedCell((cell % 2 == 0 ? cell + '' : '13,659'), '0%', true);
+    const cell: React.ReactElement<{}> = (typeof cellInfo == 'string') ? <div>{cellInfo}</div> : formatNumber(cellInfo);
+    return (
+        <td key={index}>
+            {cell}
+        </td>
+    );
+}
+
+const labels = group => group.footer.map(() => <td>{splitedCell('wgt', 'cnt', false)}</td>);
+
+export default function createGroup(group: Group, onGroupCollapse, removeGroup) {
+    const style = group.isCollapsed ? { display: 'none' } : {};
+    return [
+        (<tr className="group-title">
+            <td>{collapsed(group.isCollapsed, () => onGroupCollapse(group))}
+                {group.name}
+                <span onClick={() => removeGroup(group)} className="glyphicon glyphicon-remove"></span>
+            </td>
+            {labels(group)}
+        </tr>),
+        (
+            [group.answers.map((a, i) => (
+                <tr key={i} className="subgroup" style={style}>
+                    {a.values.map(cell)}
+                </tr>
+            )),
+            (<tr style={style}>
+                <td><b>Footer</b></td>
+                {group.footer.map(cell)}
+            </tr>)
+            ]
+        )
+    ];
+}
--- src/App/RichTable/RichTable.tsx
+++ src/App/RichTable/RichTable.tsx
@@ -1,57 +1,7 @@
 import * as React from 'react';
-import * as cx from 'classnames';
 
 import { Group, Report, SortingIndicator } from '../../types/report';
-
-const collapsed = (isCollapsed, onGroupCollapse) => isCollapsed ?
-    <span onClick={onGroupCollapse} className="glyphicon glyphicon-plus"></span> :
-    <span onClick={onGroupCollapse} className="glyphicon glyphicon-minus"></span>;
-
-const splitedCell = (leftValue, rightValue, bordered) => (
-    <div className="wrapper">
-        <span className={cx('firstCell', { borderedCell: bordered })}>{leftValue}</span>
-        <span className="secondCell">{rightValue}</span>
-    </div>
-);
-
-const cell = (cellInfo: string | number, index: number) => {
-    const formatNumber = (cell: number) => splitedCell((cell % 2 == 0 ? cell + '' : '13,659'), '0%', true);
-    const cell: React.ReactElement<{}> = (typeof cellInfo == 'string') ? <div>{cellInfo}</div> : formatNumber(cellInfo);
-    return (
-        <td key={index}>
-            {cell}
-        </td>
-    );
-}
-
-function group(group: Group, onGroupCollapse, removeGroup) {
-
-    const style = group.isCollapsed ? { display: 'none' } : {};
-    return [
-        (<tr className="group-title">
-            <td>{collapsed(group.isCollapsed, () => onGroupCollapse(group))}
-                {group.name}
-                <span onClick={() => removeGroup(group)} className="glyphicon glyphicon-remove"></span>
-            </td>
-            <td>{splitedCell('wgt', 'cnt', false)}</td>
-            <td>{splitedCell('wgt', 'cnt', false)}</td>
-            <td>{splitedCell('wgt', 'cnt', false)}</td>
-        </tr>),
-        (
-            [group.answers.map((a, i) => (
-                <tr key={i} className="subgroup" style={style}>
-                    {a.values.map(cell)}
-                </tr>
-            )),
-            (<tr style={style}>
-                <td><b>Footer</b></td>
-                {group.footer.map(cell)}
-            </tr>)
-            ]
-        )
-    ];
-}
-//TODO: extract 'Footer' as param
+import createGroup from './Group';
 
 const sortingIndicator = (sorter: SortingIndicator) =>
     sorter == SortingIndicator.Asc ? <span className="glyphicon glyphicon-chevron-up"></span> :
@@ -76,7 +26,7 @@ export default (props: { report: Report, onSort: SortingCallback, onGroupCollaps
             {header(props.report, props.onSort)}
         </thead>
         <tbody>
-            {props.report.groups.map(g => group(g, props.onGroupCollapse, props.removeGroup))}
+            {props.report.groups.map(g => createGroup(g, props.onGroupCollapse, props.removeGroup))}
         </tbody>
     </table>
 );
