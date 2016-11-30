351edd49000edf29ef9e6f213764930dc66e47e1
Ilya Ivanov <static.ila@gmail.com>
2016-11-30 15:03:18 +0200

    Add missing App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as _ from 'lodash';
 import { NotificationStack } from 'react-notification';
 
 import RichTable from './RichTable/RichTable';
@@ -9,34 +10,39 @@ import createReport from './model/report';
 import { removeColumn, sort, toggleCollapsed, removeGroup, swapColumns } from './model/operations';
 import createHeaders from './summary/headers';
 
-import { Report } from './types/report';
+import { Report, Group } from './types/report';
 
-import './styles/bootstrap-isolated.scss';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './styles/styles.scss';
 
 interface State { report: Report, notifications: any[], newNotificationId: number }
 
 export default class App extends React.Component<{}, State>{
 
-  // addNotification(message, isError) {
-  //   const newCount = this.state.newNotificationId + 1;
-  //   return this.setState({
-  //     notifications: this.state.notifications.concat([{
-  //       message,
-  //       key: newCount,
-  //       className: isError ? "error-notification" : "",
-  //       action: 'Dismiss',
-  //       onClick: () => this.removeNotification(newCount),
-  //       dismissAfter: 4000
-  //     }]),
-  //     newNotificationId: newCount
-  //   });
-  // }
+  addNotification(message) {
+    const newCount = this.state.newNotificationId + 1;
+    this.setState({
+      notifications: this.state.notifications.concat([{
+        message,
+        key: newCount,
+        report: _.cloneDeep(this.state.report),
+        action: 'UNDO',
+        onClick: () => this.revertToNotificationState(newCount),
+        dismissAfter: 10000
+      }]),
+      newNotificationId: newCount
+    } as State);
+  }
 
-  // removeNotification(id) {
-  //   this.setState({ notifications: this.state.notifications.filter(n => n.key !== id) });
-  // }
+  revertToNotificationState(id) {
+    const notification = this.state.notifications.filter(n => n.key === id)[0];
+    this.setState({ report: notification.report } as State);
+    this.setState({ notifications: _.takeWhile(this.state.notifications, n => n.key !== id) } as State);
+  }
 
+  dismissNotificaiton(id) {
+    this.setState({ notifications: this.state.notifications.filter(n => n.key !== id) } as State);
+  }
 
   constructor(props) {
     super(props);
@@ -44,26 +50,29 @@ export default class App extends React.Component<{}, State>{
   }
 
   sort = (columnIndex: number) => {
-    console.log('sort')
-    // this.addNotification('SwappedColumns', false);
-    this.setState({ report: sort(this.state.report, columnIndex) });
+    this.setState({ report: sort(this.state.report, columnIndex) } as State);
   }
 
 
   toggle = (group) =>
-    this.setState({ report: toggleCollapsed(this.state.report, group) });
+    this.setState({ report: toggleCollapsed(this.state.report, group) } as State);
 
 
-  remove = (group) =>
-    this.setState({ report: removeGroup(this.state.report, group) });
+  remove = (group: Group) => {
+    this.addNotification(`${group.name} removed`);
+    this.setState({ report: removeGroup(this.state.report, group) } as State);
+  }
 
 
-  removeColumn = (index) =>
-    this.setState({ report: removeColumn(this.state.report, index) });
+  removeColumn = (index) => {
+    const header = this.state.report.columns[index];
+    this.addNotification(`${header.name} removed`);
+    this.setState({ report: removeColumn(this.state.report, index) } as State);
+  }
 
   swapColumns = (firstIndex, secondIndex) => {
 
-    this.setState({ report: swapColumns(this.state.report, firstIndex, secondIndex) });
+    this.setState({ report: swapColumns(this.state.report, firstIndex, secondIndex) } as State);
 
   }
 
@@ -71,14 +80,20 @@ export default class App extends React.Component<{}, State>{
     const headers = createHeaders(this.state.report, this.sort, this.removeColumn);
     const dragabbleHeaders = <DragableHeaders headers={headers} moveCard={this.swapColumns} />;
     return (
-      <div className="bootstrap-context">
-        <Menu />
-        <RichTable
-          report={this.state.report}
-          headers={dragabbleHeaders}
-          onGroupCollapse={this.toggle}
-          removeGroup={this.remove} />
+      <div>
+        <div>
+          <Menu />
+          <RichTable
+            report={this.state.report}
+            headers={dragabbleHeaders}
+            onGroupCollapse={this.toggle}
+            removeGroup={this.remove} />
 
+        </div>
+        <NotificationStack
+          notifications={this.state.notifications}
+          onDismiss={notification => this.dismissNotificaiton(notification.key)}
+          />
       </div>
     )
   }
